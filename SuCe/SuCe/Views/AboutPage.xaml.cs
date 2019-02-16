306e491890f5f0dcec41b9708ed4ce484aca68ed
Felipe Novaes <felix3designer@gmail.com>
2019-02-16 11:45:42 -0200

    Inicio Projeto SuCe

---
+++ SuCe/SuCe/Views/AboutPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SuCe.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
